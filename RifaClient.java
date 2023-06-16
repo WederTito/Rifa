d0e67b82b74545c35dd5802df4fb193ac2484993
WederTito <wedertito8@gmail.com>
2023-06-16 01:06:52 -0300

    Correção

--- RifaClient.java
+++ RifaClient.java
@@ -1,16 +1,29 @@
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 public class RifaClient {
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
             RifaService rifaService = (RifaService) registry.lookup("RifaService");
-            rifaService.comprarNumero("Cliente 1");
-            rifaService.comprarNumero("Cliente 2");
+
+            solicitarNomeEComprarNumero(rifaService);
+            solicitarNomeEComprarNumero(rifaService);
+            solicitarNomeEComprarNumero(rifaService);
+            solicitarNomeEComprarNumero(rifaService);
+
             rifaService.realizarSorteio();
         } catch (Exception e) {
             System.err.println("Erro ao acessar o serviço RMI: " + e.getMessage());
         }
+    }
+
+    private static void solicitarNomeEComprarNumero(RifaService rifaService) throws RemoteException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o nome do usuário: ");
+        String nome = scanner.nextLine();
+        rifaService.comprarNumero(nome);
     }
 }
